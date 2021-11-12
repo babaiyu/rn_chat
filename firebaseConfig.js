74c8dce9c68d09ba096b543a5a6d5e50e4c21c3c
babaiyu <bayuputra9a@gmail.com>
2021-11-12 16:58:58 +0700

    Set up env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,20 +1,29 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from '@env';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAiAYJ5EBCvwWsTVSOx8Qyu5Q2D7hIZvns',
-  authDomain: 'learn-socket-fdfaf.firebaseapp.com',
-  databaseURL:
-    'https://learn-socket-fdfaf-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'learn-socket-fdfaf',
-  storageBucket: 'learn-socket-fdfaf.appspot.com',
-  messagingSenderId: '131011713101',
-  appId: '1:131011713101:web:ebe10ad3f562cdb8242404',
-  measurementId: 'G-K8K7M2TL9F',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
